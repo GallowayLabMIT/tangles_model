4d1258c094619c3a2ebcf0a46d8f9cbbdcf02ece
Christopher Johnstone <meson800@gmail.com>
2020-08-16 14:42:55 -0400

    Updated perform_param_sweep file to distribute computation across workers

--- perform_param_sweep.py
+++ perform_param_sweep.py
@@ -2,10 +2,30 @@ import numpy as np
 import pandas as pd
 import scipy
 import sys
+import argparse
 
 import stochastic_sc_model as ssc_model
 
-def bulk_topology_simulation(geometry, dx_in_bp, expression_levels, simtime, n_simulations):
+params = {
+    'mRNA_drag': 1/20, # pN nm^(alpha / 1)
+    'mRNA_exponent': 1, # the value of alpha
+    'DNA_twist_mobility': 10, # s pN nm
+    'RNAP_radius': 15, # nm
+    'RNAP_velocity': 20, # nm / s
+    'RNAP_torque_cutoff': 12, # pN nm
+    'RNAP_stall_torque_width': 3, #pN
+    'DNA_force': 1, # pN
+    'DNA_bend_plength': 50, # pN
+    'DNA_twist_plength': 95, # pN
+    'DNA_plectonome_twist_plength': 24, # pN
+    'temperature': 298, # K
+    'base_promoter_initiation_rate': 1 / 120, # 1 / sec
+    'topo_rate': 1 / 1200, # 1 / sec
+    'mRNA_deg_rate': 1 / 1200 # 1 / sec
+}
+
+
+def bulk_topology_simulation(geometry, dx_in_bp, topo_rate_multiplier, expression_levels, simtime, n_simulations):
     """
     For a given gene geometry, with a interleaving space, runs a series of bulk simulations and
     returns the output dataframe.
@@ -17,13 +37,15 @@ def bulk_topology_simulation(geometry, dx_in_bp, expression_levels, simtime, n_s
         'convergent': Gene A and B face each other: A-> |dx| <-B
         'divergent': Gene A and B face away from each other: <-A |dx| B->
     dx_in_bp: The inter-gene spacing in base pairs.
+    topo_rate_multiplier: The topoisomerase base rate to use. This value is multiplied by 1/1200 1/s.
     expression_levels: A tuple containing expression levels for genes A and B.
     simtime: The amount of time to run the simulation for.
     n_simulations: The number of simulations to run
 
     Returns:
     --------
-    A list of summary values, which are [geometry, dx_in_bp, A_expression, A_mean, A_std, B_mean, B_std]
+    A list of summary values, which are
+    [geometry, dx_in_bp, topo_rate, A_expression, B_expression, A_mean, A_std, B_mean, B_std]
     """
     endcap_distances = 3000 * .34
     gene_distance = 2000 * .34
@@ -44,44 +66,41 @@ def bulk_topology_simulation(geometry, dx_in_bp, expression_levels, simtime, n_s
                  if geometry == 'convergent' else
                  (gene_B_boundaries[0], gene_B_boundaries[1], expression_levels[1], 'B'))
     
-    raw_data = ssc_model.bulk_simulation(params, barriers, genes, ['A', 'B'], (0, simtime, 1000), n_simulations)
+    params_copy = dict(params)
+    params_copy['topo_rate'] *= topo_rate_multiplier
+
+    raw_data = ssc_model.bulk_simulation(params_copy, barriers, genes, ['A', 'B'], (0, simtime, 1000), n_simulations)
     mean_data = raw_data.groupby('time').mean().reset_index()
     mean_data = mean_data[mean_data['time']==simtime]
     std_data = raw_data.groupby('time').std().reset_index()
     std_data = std_data[std_data['time']==simtime]
 
-    return [geometry, dx_in_bp, expression_levels[0], expression_levels[1],
+    return [geometry, dx_in_bp, params_copy['topo_rate'],
+            expression_levels[0], expression_levels[1],
             mean_data['A_expression'].iloc[0], std_data['A_expression'].iloc[0],
             mean_data['B_expression'].iloc[0], std_data['B_expression'].iloc[0]]
+            
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Python helper code to run a parameter sweep")
+    parser.add_argument('--runner_id', type=int, required=True, help='The runner index of this instance')
+    parser.add_argument('--num_runners', type=int, required=True, help='The total number of runners that work is divided over')
+    parser.add_argument('--n_simulations', type=int, default=5000, required=True, help='The number of simulations to run for each parameter set')
+    args = parser.parse_args()
+
     # Ensure we don't hit floating point errors
     np.seterr('raise')
     np.seterr(under='ignore')
-
-    params = {
-        'mRNA_drag': 1/20, # pN nm^(alpha / 1)
-        'mRNA_exponent': 1, # the value of alpha
-        'DNA_twist_mobility': 10, # s pN nm
-        'RNAP_radius': 15, # nm
-        'RNAP_velocity': 20, # nm / s
-        'RNAP_torque_cutoff': 12, # pN nm
-        'RNAP_stall_torque_width': 3, #pN
-        'DNA_force': 1, # pN
-        'DNA_bend_plength': 50, # pN
-        'DNA_twist_plength': 95, # pN
-        'DNA_plectonome_twist_plength': 24, # pN
-        'temperature': 298, # K
-        'base_promoter_initiation_rate': 1 / 120, # 1 / sec
-        'topo_rate': 1 / 1200, # 1 / sec
-        'mRNA_deg_rate': 1 / 1200 # 1 / sec
-    }
-
     # We are given an arg with inter-gene spacing. We will run at 10 different upstream expression levels
     accum = []
+    run_id = 0
     for geometry in ['tandem', 'convergent', 'divergent']:
         for expression in np.logspace(-2,0,10):
-            accum.append(bulk_topology_simulation(geometry, int(sys.argv[1]), (expression, 1), 12000, 5000))
-            print(f'Finished {geometry}/expression:{expression}')
-    df = pd.DataFrame(accum, columns=['geometry', 'spacing', 'A_promoter_strength', 'B_promoter_strength', 'A_mean', 'A_std', 'B_mean', 'B_std'])
+            for dx in [400, 500, 750, 1000, 1500, 2000, 2500, 3000]:
+                for topo in [1, 2, 5, 10]:
+                    if (run_id % args.num_runners) == args.runner_id:
+                        accum.append(bulk_topology_simulation(geometry, dx, 1, (1, expression), 12000, args.n_simulations))
+                        print(f'Finished {geometry}/expression:{expression}/dx:{dx}/topo_multiplier:{topo}')
+                    run_id += 1
+    df = pd.DataFrame(accum, columns=['geometry', 'spacing', 'topo_rate', 'A_promoter_strength', 'B_promoter_strength', 'A_mean', 'A_std', 'B_mean', 'B_std'])
     df.to_feather(f'output/dataframes/dx{int(sys.argv[1])}.feather')
