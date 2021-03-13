912fad3d8574412d5a0ef255d516d41c323e10ff
Christopher Johnstone <meson800@gmail.com>
2021-03-13 17:18:46 -0500

    Added Tangles 2.0 runner scripts
    Fixed t2.0 sweep bug, updated to realistic sim size
    
    Rearranged tangles folder
    
    Rearranged tangles folder

--- scripts/test_new_model.py
+++ scripts/test_new_model.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
 
     genes = {"RFP": [2000, 2600]}
     promoter = [ssc.Promoter(ssc.PromoterType.STATIC, 1, 2000, 1)]
-    pas = [ssc.PAS(ssc.PAS_Type.NO_RT, 2600, 1, 10)]
+    pas = [ssc.PAS(ssc.PAS_Type.NO_RT, 2600, 1)]
     bcs = (ssc.BoundaryCondition.fixed(0), ssc.BoundaryCondition.fixed(4000))
 
     runner = ssc.SimulationRunner(genes, promoter, pas, bcs, (0, 10000))
-    print(runner.multi_simulation(5))
+    runner.multi_simulation(5)
