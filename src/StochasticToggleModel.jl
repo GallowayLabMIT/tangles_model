fede7cd6bdaca1f945ddd676f038ed6df80cf286
Christopher Johnstone <meson800@gmail.com>
2021-11-10 09:38:05 -0500

    Updated Manifest with packages needed for plotting 10.521 model
    Bumped package versions and added StochasticToggleModel submodule
    
    Bump pillow from 8.2.0 to 8.3.2
    
    Bumps [pillow](https://github.com/python-pillow/Pillow) from 8.2.0 to 8.3.2.
    - [Release notes](https://github.com/python-pillow/Pillow/releases)
    - [Changelog](https://github.com/python-pillow/Pillow/blob/master/CHANGES.rst)
    - [Commits](https://github.com/python-pillow/Pillow/compare/8.2.0...8.3.2)
    
    ---
    updated-dependencies:
    - dependency-name: pillow
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

---
+++ src/StochasticToggleModel.jl
@@ -0,0 +1,39 @@
+using DiffEqJump
+# Parameters are p = (n, K, α, β)
+n(p) = p[1]
+K(p) = p[2]
+α(p) = p[3]
+β(p) = p[4]
+rate_A_creation(u,p,_) = α(p) / (K(p) + u[2]^n(p))
+rate_A_degradation(u,p,_) = β(p) * u[1]
+rate_B_creation(u,p,_) = α(p) / (K(p) + u[1]^n(p))
+rate_B_degradation(u,p,_) = β(p) * u[2]
+
+function A_creation_affect!(integrator)
+    integrator.u[1] += 1
+end
+
+function B_creation_affect!(integrator)
+    integrator.u[2] += 1
+end
+
+function A_degradation_affect!(integrator)
+    integrator.u[1] -= 1
+end
+
+function B_degradation_affect!(integrator)
+    integrator.u[2] -= 1
+end
+
+function generate_jump_problem(ic::Vector{Int64}, tspan::Tuple{Float64,Float64}, n::Float64, K::Float64, α::Float64, β::Float64)
+    A_create_jump =  ConstantRateJump(rate_A_creation,    A_creation_affect!)
+    B_create_jump =  ConstantRateJump(rate_B_creation,    B_creation_affect!)
+    A_degrade_jump = ConstantRateJump(rate_A_degradation, A_degradation_affect!)
+    B_degrade_jump = ConstantRateJump(rate_B_degradation, B_degradation_affect!)
+
+    return JumpProblem(
+        DiscreteProblem(ic, tspan, (n, K, α, β)),
+        Direct(),
+        A_create_jump, B_create_jump, A_degrade_jump, B_degrade_jump
+    )
+end
