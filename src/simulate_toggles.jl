b9d9f72d85aac2b69383653cee03df2a0ec54724
Christopher Johnstone <meson800@gmail.com>
2021-11-12 18:22:35 -0500

    Parallel look at the amount of cooperativity that supercoiling imparts

--- src/simulate_toggles.jl
+++ src/simulate_toggles.jl
@@ -11,33 +11,52 @@ function adjust_ics(prob, i, _)
     print(ics)
     generate_jump_problem(ics, (0.0, 100000.0), 5.0, 8.0, 1/160.0, 1/1200.0)
 end
-# Interesting regime: 5.0, 8.0, 1/20.0, 1/1200.0
 
+function animate_trajectories(ensemble_solution, n_frames, tspan, dot_colors, filename)
+    timepoints = range(tspan[1], tspan[2], length=n_frames)
+    solution_3d = permutedims(cat([
+        cat(componentwise_vectors_timepoint(ensemble_solution, i)..., dims=2)
+            for i in range(tspan[1], stop=tspan[2], length=n_frames)]...,
+        dims=3),[3, 1, 2])
+    summary_animation = @animate for i ∈ 1:n_frames
+        plot(componentwise_vectors_timepoint(ensemble_solution, timepoints[i])...,
+            xlims=[0,20],
+            ylims=[0,20],
+            seriestype=:scatter, markercolor=dot_colors, alpha=0.2, label="",
+            title="Stochastic toggle switch trajectories")
+        if i > 1
+            plot!(
+                solution_3d[max(1,i - 10):i,:,1],
+                solution_3d[max(1,i-10):i,:,2],
+                alpha=range(0.08, stop=0.0, length=(i - max(0, i - 11))),
+                color="purple",
+                label="")
+        end
+    end
+    mp4(summary_animation,filename)
+end
+
+# Interesting regime: 5.0, 8.0, 1/20.0, 1/1200.0
 prob = generate_jump_problem([5,5], (0.0,100000.0), 5.0, 8.0, 1/160.0, 1/1200.0)
 ensemble_prob = EnsembleProblem(prob, prob_func=adjust_ics)
 sol= solve(ensemble_prob, SSAStepper(), trajectories=400)
-
-n_frames = 400
-timepoints = range(0.0, stop=10000.0, length=n_frames)
 dot_colors = [(i % 20) <= trunc(Int64, i / 20) ? 1 : 2 for i in 1:400]
-solution_3d = permutedims(cat([
-    cat(componentwise_vectors_timepoint(sol, i)..., dims=2) for i in range(0.0, stop=10000.0, length=n_frames)]...,
-    dims=3),[3, 1, 2])
-summary_animation = @animate for i ∈ 1:n_frames
-    plot(componentwise_vectors_timepoint(sol, timepoints[i])...,
-        xlims=[0,20],
-        ylims=[0,20],
-        seriestype=:scatter, markercolor=dot_colors, alpha=0.2, label="")
-    if i > 1
-        plot!(
-            solution_3d[max(1,i - 10):i,:,1],
-            solution_3d[max(1,i-10):i,:,2],
-            alpha=range(0.08, stop=0.0, length=(i - max(0, i - 11))),
-            color="purple",
-            label="")
-    end
+
+animate_trajectories(sol, 400, [0.0, 10000.0], dot_colors, "output/movies/stochastic_toggle_summary.mp4")
+
+# Simulate starting in one of the basins
+basin_curves = []
+for n in 1.0:5.0
+    basin_ensemble = EnsembleProblem(generate_jump_problem([8,0], (0.0,300000.0), n, 8.0, 1/160.0, 1/1200.0))
+    basin_solution = solve(basin_ensemble, SSAStepper(), trajectories=5000)
+
+    basin_percentage(t) = sum(componentwise_vectors_timepoint(basin_solution, t)[1] .> componentwise_vectors_timepoint(basin_solution, t)[2]) / 5000
+    append!(basin_curves, vcat([basin_percentage(t) for t in range(0.0, stop=300000.0, length=200)]...))
 end
-mp4(summary_animation,"output/movies/stochastic_toggle_summary.mp4")
+basin_curves = convert(Matrix{Float64},reshape(basin_curves, (200,5)))
+plot(basin_curves)
+
+# Start simulation in one area
 
 #====================================TANGLES=======================================================
 ==================================================================================================#
@@ -70,12 +89,3 @@ low_coupling_problem = TanglesModel.build_problem(
 tangles_soln = low_coupling_problem()
 mRNA_vals = hcat([tangles_soln.u[i].u.mRNA for i in 1:length(tangles_soln)]...)
 plot(transpose(mRNA_vals))
-
-        for (index, val) in enumerate(solution.u)
-            x = val.u.x[1:end-1]
-            n_polymerases = convert(Int, length(x) / 3)
-            rnap_loc[index,1:n_polymerases] = x[1:3:end]
-            ϕ[index,1:n_polymerases] = x[2:3:end]
-            z[index,1:n_polymerases] = x[3:3:end]
-            mRNA[index,:] = val.u.mRNA
-        end
