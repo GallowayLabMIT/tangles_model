ed47442a38ed3734afb0b0f71dcf1846a1f9cb0a
Christopher Johnstone <meson800@gmail.com>
2022-02-23 17:30:17 -0500

    Updated base model examples figure

---
+++ src/merge_all_topo_sims.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import h5py
+
+import pathlib
+from datetime import datetime, timedelta
+
+if __name__ == '__main__':
+    i = 0
+    with h5py.File(f'../output/modeling_paper/modeling_toggles_combined.h5','w') as outfile:
+        for f in pathlib.Path('../output/modeling_paper/to_merge').glob(f'fig_toggles_sims*.h5'):
+            outgroup = outfile.create_group(f.stem)
+            with h5py.File(f, 'r') as infile:
+                for group in infile.keys():
+                    if group.startswith('tangles_discrete_run'):
+                        infile.copy(infile[group], outgroup, f'tangles_discrete_run.{i:08}')
+                        i += 1
+            print(f'Done with {f.stem}')
+    with h5py.File(f'../output/modeling_paper/modeling_toggles_topo_combined.h5','w') as outfile:
+        for f in pathlib.Path('../output/modeling_paper/to_merge').glob(f'fig_toggles_topo_sims*.h5'):
+            outgroup = outfile.create_group(f.stem)
+            with h5py.File(f, 'r') as infile:
+                for group in infile.keys():
+                    if group.startswith('tangles_discrete_run'):
+                        infile.copy(infile[group], outgroup, f'tangles_discrete_run.{i:08}')
+                        i += 1
+            print(f'Done with {f.stem}')
---
+++ src/merge_bm_examples.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import h5py
+
+import pathlib
+from datetime import datetime, timedelta
+
+if __name__ == '__main__':
+    i = 0
+    with h5py.File(f'../output/modeling_paper/modeling_bm_examples_combined.h5','w') as outfile:
+        for f in pathlib.Path('../output/modeling_paper').glob(f'fig_bm_examples_sims_*.h5'):
+            outgroup = outfile.create_group(f.stem)
+            with h5py.File(f, 'r') as infile:
+                for group in infile.keys():
+                    if group.startswith('tangles_discrete_run'):
+                        infile.copy(infile[group], outgroup, f'tangles_discrete_run.{i:08}')
+                        i += 1
+            print(f'Done with {f.stem}')
