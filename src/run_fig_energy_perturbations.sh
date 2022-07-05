eb3977e9bec8ebc1bbd5e68bbf8312e0026b298f
Christopher Johnstone <meson800@gmail.com>
2022-07-05 18:00:44 -0400

    Added perturbative simulations

---
+++ src/run_fig_energy_perturbations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Launch with LLsub src/run_fig_energy_perturbations.sh [10,60,1]
+source /etc/profile
+module load julia/1.6.1
+
+julia --project=. src/simulate_fig_energy_perturbations.jl $LLSUB_RANK $LLSUB_SIZE
