3e811746aa189276f8748cb55921b08ba29cb71c
Christopher Johnstone <meson800@gmail.com>
2022-07-11 18:14:27 -0400

    Updated energy perturbation sim to simulate SC/RNAP dynamics at specific induction levels

--- src/simulate_fig_energy_perturbations.jl
+++ src/simulate_fig_energy_perturbations.jl
@@ -42,9 +42,9 @@ end
 for _ in 1:n_repeats,
     is_plasmid in [false, true],
     sc_initiation in [true],
-    induction in exp10.(range(-2,0.5,length=30)),
+    induction in vcat([0.0,1.0],exp10.(range(-2,0.5,length=30))),
     σ2 in [0.025],
-    scenario in ["buffering", "energy_well"]
+    scenario in ["none", "buffering", "energy_well"]
 
 
     if i % n_nodes != node_idx
@@ -73,10 +73,12 @@ for _ in 1:n_repeats,
     simulate_summarized_runs(filename, n_summarized_runs_per_repeat, "fig.perturb.tandem_reporter_downstream", params, bcs, tandem_reporter_down, 15000.0)
     simulate_summarized_runs(filename, n_summarized_runs_per_repeat, "fig.perturb.convergent", params, bcs, convergent, 15000.0)
     simulate_summarized_runs(filename, n_summarized_runs_per_repeat, "fig.perturb.divergent", params, bcs, divergent, 15000.0)
-    simulate_sc_rnap_dynamics(filename, n_sc_burst_runs_per_repeat, "fig.perturb.sc_bursts.tandem_reporter_upstream", params, bcs, tandem_reporter_up, 200, 15000.0, 20, Dict{String,Float64}())
-    simulate_sc_rnap_dynamics(filename, n_sc_burst_runs_per_repeat, "fig.perturb.sc_bursts.tandem_reporter_downstream", params, bcs, tandem_reporter_down, 200, 15000.0, 20, Dict{String,Float64}())
-    simulate_sc_rnap_dynamics(filename, n_sc_burst_runs_per_repeat, "fig.perturb.sc_bursts.convergent", params, bcs, convergent, 200, 15000.0, 20, Dict{String,Float64}())
-    simulate_sc_rnap_dynamics(filename, n_sc_burst_runs_per_repeat, "fig.perturb.sc_bursts.divergent", params, bcs, divergent, 200, 15000.0, 20, Dict{String,Float64}())
+    if (induction == 0.0) || (induction == 1.0)
+        simulate_sc_rnap_dynamics(filename, n_sc_burst_runs_per_repeat, "fig.perturb.sc_bursts.tandem_reporter_upstream", params, bcs, tandem_reporter_up, 200, 15000.0, 20, Dict{String,Float64}())
+        simulate_sc_rnap_dynamics(filename, n_sc_burst_runs_per_repeat, "fig.perturb.sc_bursts.tandem_reporter_downstream", params, bcs, tandem_reporter_down, 200, 15000.0, 20, Dict{String,Float64}())
+        simulate_sc_rnap_dynamics(filename, n_sc_burst_runs_per_repeat, "fig.perturb.sc_bursts.convergent", params, bcs, convergent, 200, 15000.0, 20, Dict{String,Float64}())
+        simulate_sc_rnap_dynamics(filename, n_sc_burst_runs_per_repeat, "fig.perturb.sc_bursts.divergent", params, bcs, divergent, 200, 15000.0, 20, Dict{String,Float64}())
+    end
 
     println("Done with perturbation sims with params:\n\tis_plasmid: ", is_plasmid, "\n\tinduction: ", induction, "\n\tscenario: ", scenario)
     println("Ran round in ", time() - start_time, " seconds")
