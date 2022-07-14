7768229b09ec0c21c1066dd190e58c5ae346fab7
Christopher Johnstone <meson800@gmail.com>
2022-07-14 11:50:58 -0400

    Added simplified Zinani (CymR) simulations

---
+++ src/run_simplified_zinani_slurm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Launch with LLsub src/run_simplified_zinani_slurm.sh [10,60,1]
+source /etc/profile
+module load julia/1.6.1
+
+julia --project=. src/simulate_simplified_zinani.jl $LLSUB_RANK $LLSUB_SIZE
